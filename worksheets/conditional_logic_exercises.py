e26f07755e2bfb473ae96accd6c29148a7af04f5
nejutiii <hussenshimelsabera2@gmail.com>
2025-10-22 11:30:22 -0700

    Add age classification based on user input

--- worksheets/conditional_logic_exercises.py
+++ worksheets/conditional_logic_exercises.py
@@ -1,2 +1,10 @@
-print("Conditional Logic Exercises")
-# Practice if/elif/else statements here.
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You are a child.")
+elif age < 20:
+    print("You are a teenager.")
+elif age < 60:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
